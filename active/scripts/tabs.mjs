a10212e7dbc537288d5415c08237384c34ab45d7
Bubber <bubbertheclayblobofficial@gmail.com>
2026-01-28 16:42:03 -0500

    Update tabs.mjs

--- active/scripts/tabs.mjs
+++ active/scripts/tabs.mjs
@@ -51,7 +51,7 @@ pageRefresh.onclick = () => {
 };
 
 newTabButton.onclick = () => {
-  addTab("html.duckduckgo.com/html");
+  addTab("duckduckgo.com");
 };
 
 // Options (opt menu)
@@ -133,7 +133,7 @@ const tabItem = (tab) => {
             if (tabs.length) focusTab(tabs[tabs.length - 1]);
             else
               setTimeout(() => {
-                addTab("html.duckduckgo.com/html");
+                addTab("duckduckgo.com");
               }, 100);
           }
 
@@ -248,7 +248,7 @@ async function addTab(link) {
   focusTab(tab);
 }
 
-addTab("html.duckduckgo.com/html");
+addTab("duckduckgo.com");
 
 const urlParams = new URLSearchParams(window.location.search);
 
